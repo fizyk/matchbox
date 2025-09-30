8651539533c79c98f504cdf1678948c2c490d677
Grzegorz Śliwiński <fizyk@fizyk.dev>
2025-09-30 15:40:26 +0200

    Replace black with ruff-format

--- matchbox/box.py
+++ matchbox/box.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with matchbox.  If not, see <http://www.gnu.org/licenses/>.
 """Match box - for indexing objects by their fields."""
+
 from collections.abc import Hashable
 from dataclasses import dataclass
 from typing import Generic, Iterable, Set
